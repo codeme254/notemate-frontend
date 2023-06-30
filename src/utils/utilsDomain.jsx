6d280a57175cf99f098616a6d17ae80e83b8f7d4
codeme254 <otwomadennis365@gmail.com>
2023-06-30 16:51:10 +0300

    fix static web app file

--- src/utils/utilsDomain.jsx
+++ src/utils/utilsDomain.jsx
@@ -1,2 +1,5 @@
 const apiDomain = "https://notemateapi.azurewebsites.net"
 export default apiDomain;
+
+// https://thankful-river-084704d0f.3.azurestaticapps.net/sign-up
+// https://notemateapi.azurewebsites.net/
